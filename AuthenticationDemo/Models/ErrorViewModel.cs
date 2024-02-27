c2a4f3a84c489e418ace5c1a18358584ca109d69
shivam <shivam@southpawstudio.co>
2024-02-27 19:14:06 +0530

    init

---
+++ AuthenticationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AuthenticationDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
